8db3e156b668002d4dc28f0dbb03d69ddb02bdd1
Thinula Girihagama <thinulahansana1122@gmail.com>
2025-04-21 08:36:15 +0530

    Home Page

---
+++ Frontend/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    
+  }
+}
